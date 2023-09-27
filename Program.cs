827b6a191ad357e74a846f5870986c03037843ff
C5PC5 <C5PC5@C5PC5>
2023-09-27 20:27:29 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿////long Peterburg = 5598486;
+////long Moscow = 13015126;
+////Console.WriteLine($"Итого в Петербурге меньше жителей на : {Moscow - Peterburg}");
+
+//decimal WorkerEarnedInAYear = 26.054856895m;
+//decimal OneMouth = 12;
+//decimal NDFL = 13;
+//decimal WorkerEarnedInAMouth = WorkerEarnedInAYear / OneMouth;
+//decimal WorkerEarnedInAMouthWithNDFL = WorkerEarnedInAYear / OneMouth % NDFL;
+//Console.WriteLine($"Worker Earning Money in a Mouth wih 13%NDFL:{WorkerEarnedInAYear/OneMouth%NDFL}");
+
+
+
+
+
